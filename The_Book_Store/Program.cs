2027b1147f89f548dc2f19825db3e0e5083bf8a6
Vid3107 <navidmeng3107@gmail.com>
2023-10-29 15:51:27 +0700

    FormAdmin and FormPublisher Done!

--- The_Book_Store/Program.cs
+++ The_Book_Store/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Book_Storee;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace The_Book_Store
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formAdmin());
         }
     }
 }
